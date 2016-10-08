fd7a58476268ba17deeec765f9d3ea02f5e68973
Nauman Umer <u.nauman@outlook.com>
2016-10-08 19:56:24 +0500

    Initialized docs component

--- app/assets/js/DocsComponent.js
+++ app/assets/js/DocsComponent.js
@@ -1,16 +1,21 @@
 var showdown  = require('showdown'),
     path = require('path'),
+    fs = require('fs'),
     converter = new showdown.Converter();
 
 MainDockerLayout.registerComponent( 'docs', function( container, state ){
     container.getElement().html('<div class="doc-body" id="'+state.id+'"></div>');
     InitializeDocs(state.id, state.name);
 });
-
 function InitializeDocs(id, name){
-    var text = getTextFormFile(getDocpath(name));
+    var text = getTextFormFile("D:\\Documents\\Nauman Umer\\New folder\\easybasic\\README.md");//getDocpath(name));
+    var html = converter.makeHtml(text)
+    console.log('#'+id);
+    $('#'+id).append(html);
 }
-
 function getDocpath(name){
-    return path.join(__dirname, '../node_modules/monaco-editor/min')
+    return path.join(__dirname, `./app/min${name}`)
+}
+function getTextFormFile(path, encoding = "utf8"){
+    return fs.readFileSync(path, encoding);
 }
